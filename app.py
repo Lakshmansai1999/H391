d390277bce32029977909a2e4a5d82064b42f6ee
Lakshmansai1999 <klakshmansai@stratapps.com>
2023-10-13 16:16:40 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import wget
+#import cv2
+import shutil
+
+import sys
+# image_url = "https://tyreehouseplans.com/wp-content/uploads/2017/07/front.jpg"
+image_url = "https://xamplify.io/assets/images/xamplify-logo.png"
+
+weburl_input= "https://xamplify.in/login"
+
+
+image_filename = wget.download(image_url)
+#print(image_filename)
+
+#cv2.imwrite('C:\\Users\\klakshmansai\\Desktop\\project1\\res\\image.png', image_filename)
+
+file_to_copy = "xamplify-logo.png"
+destination_directory = "C:\\Users\\klakshmansai\\Desktop\\project1\\res\\image.png"
+
+print('Image downloaded successfully:', image_filename)
+
+shutil.copy(file_to_copy, destination_directory)
+
+
+
+# Define the JavaScript code as a string
+js_code = f"""
+var weburl_input = "{weburl_input}";
+console.log(weburl_input);
+"""
+
+# Specify the file name and path
+file_name = r"C:\Users\klakshmansai\Desktop\project1\www\js\constanconfig.js"
+
+# Write the JavaScript code to the file
+with open(file_name, "w") as js_file:
+    js_file.write(js_code)
+
+print(f"JavaScript file '{file_name}' created successfully.")
